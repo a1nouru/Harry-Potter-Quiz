7a544b56ee0ea508f2e109b986f6412ff43b36e4
Nouru Muneza <nizzogentl@gmail.com>
2018-01-01 16:06:00 -0800

    Update spell.rb

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -36,39 +36,67 @@ class Spell
 
   # To get access to the collaborative repository, complete the methods below.
 
-  # Spell 1: Reverse
-  # This instance method should return the reversed name of a spell
-  # Tests: `bundle exec rspec -t reverse .`
-  def reverse_name
-    'write this method'
-  end
+# Spell 1: Reverse
+# This instance method should return the reversed name of a spell
+# Tests: `bundle exec rspec -t reverse .`
+def reverse_name
+    
+    return name.reverse
+end
 
-  # Spell 2: Counter
-  # This instance method should return the number
-  # (integer) of mentions of the spell.
-  # Tests: `bundle exec rspec -t counter .`
-  def mention_count
-    'write this method'
-  end
+# Spell 2: Counter
+# This instance method should return the number
+# (integer) of mentions of the spell.
+# Tests: `bundle exec rspec -t counter .`
+def mention_count
+    countMentions = 0
+    dataArr = Mention.data
+    
+    for data in dataArr
+        if name == data["Spell"] #if occurrence
+            countMentions += 1 #incrementing count
+            
+        end
+    end
+    
+    return countMentions
+end
 
-  # Spell 3: Letter
-  # This instance method should return an array of all spell names
-  # which start with the same first letter as the spell's name
-  # Tests: `bundle exec rspec -t letter .`
-  def names_with_same_first_letter
-    ['write this method']
-  end
+# Spell 3: Letter
+# This instance method should return an array of all spell names
+# which start with the same first letter as the spell's name
+# Tests: `bundle exec rspec -t letter .`
+def names_with_same_first_letter
+    arrSpell = []
+    spellData = Spell.data
+    
+    for char in spellData
+        if name[0] == char["Spell(Lower)"][0] #comparing character by character so if same put spell into arrSpell
+            arrSpell.push(char["Spell(Lower)"]) #Inserting spells with similar first character to arrSpell
+        end
+    end
+    
+    return arrSpell
+end
 
-  # Spell 4: Lookup
-  # This class method takes a Mention object and
-  # returns a Spell object with the same name.
-  # If none are found it should return nil.
-  # Tests: `bundle exec rspec -t lookup .`
-  def self.find_by_mention(mention)
-    Spell.new({"Classification" => 'write this method',
-               "Effect" => 'write this method',
-               "Spell(Lower)" => 'write this method',
-               "Spell" => 'write this method'})
-  end
+# Spell 4: Lookup
+# This class method takes a Mention object and
+# returns a Spell object with the same name.
+# If none are found it should return nil.
+# Tests: `bundle exec rspec -t lookup .`
+def self.find_by_mention(mention)
+spellData = Spell.data
+
+for i in spellData
+    if mention.name == i["Spell(Lower)"]  #Looking up member "name" of object mention from Spell.data
+        foundSpell = Spell.new({"Classification" => i["Classification"], "Effect" => i["Effect"], "Spell(Lower)" => i["Spell(Lower)"], "Spell" => i["Spell"]}) #New Spell object
+        return foundSpell #Spell object with the same name
+    end
+end
+
+return nil #If none are found
+
+end
+
 
 end
